277724255947d9e56107a720ba41204fc5c5532c
clarklindev <clarklindev@gmail.com>
2024-08-19 11:48:33 +0800

    React Hook from React context

--- react - context-app.js
+++ react - context-app.js
@@ -1,38 +1,39 @@
-import { SessionProvider } from "next-auth/react"; //next-auth @4
+// import { SessionProvider } from "next-auth/react"; //next-auth @4
 
-import Head from "next/head";
+// import Head from "next/head";
 
-import Layout from "@/components/layout/layout";
-import "@/styles/globals.css";
+// import Layout from "@/components/layout/layout";
+// import "@/styles/globals.css";
 import { NotificationContextProvider } from "@/store/notification-context";
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
   // Determine which layout to use based on the page
-  const getLayout =
-    Component.getLayout ||
-    ((page) => (
-      <Layout>
-        <Head>
-          <title>default title for all pages</title>
-          <meta
-            name="description"
-            content="default description for all pages"
-          />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        {page}
-      </Layout>
-    ));
+
+  // const getLayout =
+  // Component.getLayout ||
+  // ((page) => (
+  // <Layout>
+  // <Head>
+  // <title>default title for all pages</title>
+  // <meta
+  // name="description"
+  // content="default description for all pages"
+  // />
+  // <meta
+  // name="viewport"
+  // content="initial-scale=1.0, width=device-width"
+  // />
+  // </Head>
+  // {page}
+  // </Layout>
+  // ));
 
   return (
-    <SessionProvider session={session}>
-      <NotificationContextProvider>
-        {getLayout(<Component {...pageProps} />)}
-      </NotificationContextProvider>
-    </SessionProvider>
+    // <SessionProvider session={session}>
+    <NotificationContextProvider>
+      {/* {getLayout(<Component {...pageProps} />)} */}
+    </NotificationContextProvider>
+    // </SessionProvider>
   );
 }
 
--- react - context-usage.js
+++ react - context-usage.js
@@ -1,33 +1,39 @@
-import { useContext } from 'react';
-
-import classes from './notification.module.css';
-import NotificationContext from '../../store/notification-context';
+import { useContext } from "react";
+//note: commented out irrelevant code..
+// import classes from './notification.module.css';
+import NotificationContext from "../../store/notification-context";
+//note: NotificationContext is defined in the context...its the functions and variables to expose
+// const NotificationContext = createContext({
+//   showNotification: function (notificationData) {},
+//   hideNotification: function () {},
+//   addBlogPost: function (text) {},
+// });
 
 function Notification(props) {
   const notificationCtx = useContext(NotificationContext);
 
-  const { title, message, status } = props;
+  // const { title, message, status } = props;
 
-  let statusClasses = '';
+  // let statusClasses = "";
 
-  if (status === 'success') {
-    statusClasses = classes.success;
-  }
+  // if (status === "success") {
+  //   statusClasses = classes.success;
+  // }
 
-  if (status === 'error') {
-    statusClasses = classes.error;
-  }
+  // if (status === "error") {
+  //   statusClasses = classes.error;
+  // }
 
-  if (status === 'pending') {
-    statusClasses = classes.pending;
-  }
+  // if (status === "pending") {
+  //   statusClasses = classes.pending;
+  // }
 
-  const activeClasses = `${classes.notification} ${statusClasses}`;
+  // const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
     <div className={activeClasses} onClick={notificationCtx.hideNotification}>
-      <h2>{title}</h2>
-      <p>{message}</p>
+      {/* <h2>{title}</h2>
+      <p>{message}</p> */}
     </div>
   );
 }
